b86856182963321cdcd26fa52570e4d2d830bfef
puffer <kevin@alavik.se>
2023-09-06 20:32:01 +0200

    Update cpu.c

--- src/lib/cpu.c
+++ src/lib/cpu.c
@@ -1,16 +1,21 @@
 #include "cpu.h"
-
-void initializeCPU(CPU *cpu, Memory *mem) {
-    // Initialize CPU registers and memory
-    cpu->A = 0;
-    cpu->PC = 0;
-    cpu->memory = mem;
-
-    initializeMemory(mem);  // Initialize Memory
-}
+#include <stdio.h>
 
 void executeInstruction(CPU *cpu, Memory *mem, const char *instruction) {
-    // Implement CPU instruction execution logic here
-    // For example, you can fetch instructions and data from Memory
-    // and execute them based on the instruction set.
+    if (strcmp(instruction, "ADD") == 0) {
+        uint8_t reg1 = mem->read(cpu->PC + 1);
+        uint8_t reg2 = mem->read(cpu->PC + 2);
+        cpu->R[reg1] += cpu->R[reg2];
+        cpu->PC += 3;
+    } else if (strcmp(instruction, "JMP") == 0) {
+        uint16_t targetAddress = mem->read16(cpu->PC + 1);
+        cpu->PC = targetAddress;
+    } else {
+        printf("Unsupported instruction: %s\n", instruction);
+    }
+}
+
+void initializeCPU(CPU *cpu, Memory *mem) {
+    memset(cpu, 0, sizeof(CPU));
+    cpu->PC = 0x0000;
 }
